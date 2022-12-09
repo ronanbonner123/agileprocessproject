e22340f410fda0572951c0964a279705134c63b1
3E0 <111310913+3E0@users.noreply.github.com>
2022-12-09 16:26:39 +0000

    Update main.py
    removed # text from github

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-# This will be our main file for integrating strings of code for our project
-# This workflow will install Python dependencies & lint with a variety of Python version 3.11
-# For more information see: https://docs.github.com/en/actions/automating-builds-and-tests/building-and-testing-python
 def read_integer_between_numbers(prompt, mini, maximum):
     while True:
         try:
